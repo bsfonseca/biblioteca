849cc85a8272141a45c764b5e4a1b68d48e02af6
bsfonseca <bs.fonseca12@gmail.com>
2023-10-30 18:35:23 -0300

    Atualização de livros, estilização da página

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ interface Livro {
     titulo: string;
     autor: string;
     anoPublicacao: number;
-    dataCadastro: number;
+    dataCadastro: Date;
     genero: string;
     descricao: string;
 }
@@ -19,7 +19,7 @@ function App() {
             titulo: "Admirável mundo novo",
             autor: "Aldous Huxley",
             anoPublicacao: 2010,
-            dataCadastro: 2011,
+            dataCadastro: new Date(2011, 2, 10),
             genero: "drama",
             descricao: "Livro de drama comovente com histórias horripilantes",
         },
@@ -27,40 +27,63 @@ function App() {
             id: 2,
             titulo: "Eu não sei quem é você",
             autor: "Penny Hancock",
-            anoPublicacao: 2015,
-            dataCadastro: 2015,
+            anoPublicacao: 2010,
+            dataCadastro: new Date(2012, 6, 11),
             genero: "drama",
             descricao: "Duas amigas, uma acusação e incontáveis consequências",
         },
     ]);
 
-    const [id, setId] = useState("");
+    const [id, setId] = useState(-1);
 
     const [titulo, setTitulo] = useState("");
 
     const [autor, setAutor] = useState("");
 
-    const [anoPublicacao, setAnoPublicacao] = useState(Number);
+    const [anoPublicacao, setAnoPublicacao] = useState<number>(2023);
 
-    const [dataCadastro, setDataCadastro] = useState(Number);
+    const [dataCadastro, setDataCadastro] = useState("");
 
     const [genero, setGenero] = useState("");
 
     const [descricao, setDescricao] = useState("");
 
+    const lerNomeBotao = () => {
+        if (id === -1) {
+            return "Adicionar";
+        }
+
+        return "Salvar";
+    };
+
+    const dataAtual = new Date();
+
     function adicionarLivro(event: any) {
         event.preventDefault();
-        if (!id || !titulo || !autor || !anoPublicacao || !dataCadastro || !genero || !descricao) {
+        if (!titulo || !autor || !anoPublicacao || !genero || !descricao) {
             alert("Preencha todos os campos");
             return;
         }
 
+        if (anoPublicacao > dataAtual.getFullYear()) {
+            alert("O ano de publicação informado é maior que o atual");
+            return;
+        }
+
+        if (id >= 0) {
+            salvarLivro(id, titulo, autor, anoPublicacao, genero, descricao);
+            return;
+        }
+        if (!dataCadastro) {
+            alert("A data de cadastro não foi preechida");
+            return;
+        }
         const novoLivro = {
             id: livros.length + 1,
             titulo,
             autor,
             anoPublicacao,
-            dataCadastro,
+            dataCadastro: new Date(dataCadastro),
             genero,
             descricao,
         };
@@ -70,8 +93,8 @@ function App() {
         setId(id);
         setTitulo("");
         setAutor("");
-        setAnoPublicacao(Number);
-        setDataCadastro(Number);
+        setAnoPublicacao(dataAtual.getFullYear());
+        setDataCadastro(Date);
         setGenero("");
         setDescricao("");
     }
@@ -81,6 +104,7 @@ function App() {
 
         setLivros(livrosAtualizados);
     }
+
     function atualizarLivro(id: number) {
         const livro = livros.find((item) => item.id == id);
         if (!livro) {
@@ -96,60 +120,115 @@ function App() {
         setDescricao(livro.descricao);
     }
 
+    function salvarLivro(
+        id: number,
+        titulo: string,
+        autor: string,
+        anoPublicacao: number,
+        genero: string,
+        descricao: string
+    ) {
+        const indice = livros.findIndex((item) => item.id == id);
+
+        if (indice < 0) {
+            alert("Livro não encontrado");
+            return;
+        }
+
+        livros[indice].titulo = titulo;
+        livros[indice].autor = autor;
+        livros[indice].anoPublicacao = anoPublicacao;
+        livros[indice].genero = genero;
+        livros[indice].descricao = descricao;
+
+        setLivros(livros);
+
+        setTitulo("");
+        setAutor("");
+        setAnoPublicacao(dataAtual.getFullYear());
+        setGenero("");
+        setDescricao("");
+    }
+
     return (
         <div>
-            <h1>Biblioteca</h1>
-            <hr />
-            <table>
-                <tr>
-                    <th>Título:</th>
-                    <th>Autor:</th>
-                    <th>Ano de Publicação:</th>
-                    <th>Data de Cadastro:</th>
-                    <th>Gênero:</th>
-                    <th>Descrição:</th>
-                </tr>
-                {livros.map((item) => {
-                    return (
-                        <tr>
-                            <td>{item.titulo}</td>
-                            <td>{item.autor}</td>
-                            <td>{item.anoPublicacao}</td>
-                            <td>{item.dataCadastro}</td>
-                            <td>{item.genero}</td>
-                            <td>{item.descricao}</td>
-                        </tr>
-                    );
-                })}
-            </table>
-            <hr />
+            <h1 className="titulo">Biblioteca</h1>
 
-            <form onSubmit={adicionarLivro}>
-                <h2>Adicione um novo livro</h2>
+            <table id="tabs-biblioteca">
+                <thead>
+                    <tr>
+                        <th>Título</th>
+                        <th>Autor</th>
+                        <th>Ano de Publicação</th>
+                        <th>Data de Cadastro</th>
+                        <th>Gênero</th>
+                        <th>Descrição</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {livros.map((item) => {
+                        return (
+                            <tr key={item.id}>
+                                <td>{item.titulo}</td>
+                                <td>{item.autor}</td>
+                                <td>{item.anoPublicacao}</td>
+                                <td>{item.dataCadastro.toLocaleDateString("pt-BR")}</td>
+                                <td>{item.genero}</td>
+                                <td>{item.descricao}</td>
+                                <td>
+                                    <button onClick={() => excluirLivro(item.id)}>Excluir</button>
+                                </td>
+                                <td>
+                                    <button onClick={() => atualizarLivro(item.id)}>Atualizar</button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+
+            <form id="form-biblioteca" onSubmit={adicionarLivro}>
+                <h2 className="titulo">Adicione um novo livro</h2>
                 <div>
-                    <label>Título: </label>
+                    <label>Título </label>
                     <input type="text" name="titulo" value={titulo} onChange={(e) => setTitulo(e.target.value)} />
                 </div>
                 <div>
-                    <label>Autor: </label>
+                    <label>Autor </label>
                     <input type="text" name="autor" value={autor} onChange={(e) => setAutor(e.target.value)} />
                 </div>
+
+                {id === -1 && (
+                    <div>
+                        <label>Data de Cadastro </label>
+                        <input
+                            type="date"
+                            name="data"
+                            value={dataCadastro}
+                            onChange={(e) => setDataCadastro(e.target.value)}
+                        />
+                    </div>
+                )}
+
                 <div>
-                    <label>Ano de Publicação: </label>
-                    <input type="date" name="data" value={anoPublicacao} onChange={(e) => setAutor(e.target.value)} />
+                    <label>Gênero </label>
+                    <input type="text" name="genero" value={genero} onChange={(e) => setGenero(e.target.value)} />
                 </div>
                 <div>
-                    <label>Data de Cadastro: </label>
-                    <input type="date" name="data" value={dataCadastro} onChange={(e) => setAutor(e.target.value)} />
+                    <label>Descrição </label>
+                    <input type="text" name="genero" value={descricao} onChange={(e) => setDescricao(e.target.value)} />
                 </div>
                 <div>
-                    <label>Gênero: </label>
-                    <input type="text" name="genero" value={genero} onChange={(e) => setAutor(e.target.value)} />
+                    <label>Ano de Publicação </label>
+                    <input
+                        type="number"
+                        name="data"
+                        value={anoPublicacao}
+                        onChange={(e) => setAnoPublicacao(Number(e.target.value))}
+                    />
                 </div>
-                <div>
-                    <label>Descrição: </label>
-                    <input type="text" name="genero" value={descricao} onChange={(e) => setAutor(e.target.value)} />
-                </div>
+                <button id="form-submit">{lerNomeBotao()}</button>
             </form>
         </div>
     );
